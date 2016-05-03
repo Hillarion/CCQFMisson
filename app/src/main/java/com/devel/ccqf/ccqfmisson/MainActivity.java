fa5a27c75709ede93ca647ceb646513018afc30e
username <Jonathan.Bleau-Guerette.1@collegeahuntsic.qc.ca>
2016-05-03 12:07:24 -0400

    Squelette

--- app/src/main/java/com/devel/ccqf/ccqfmisson/MainActivity.java
+++ app/src/main/java/com/devel/ccqf/ccqfmisson/MainActivity.java
@@ -1,5 +1,6 @@
 package com.devel.ccqf.ccqfmisson;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnDoc;
+    private Button btnFeed;
+    private Button btnMonAgenda;
+    private Button btnProgram;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +29,49 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+               /* Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                Intent i = new Intent(MainActivity.this, Survey.class);
+                startActivity(i);
             }
         });
+
+        btnFeed = (Button)findViewById(R.id.feedActivityBtn);
+        btnFeed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Feed.class);
+                startActivity(i);
+            }
+        });
+
+        btnDoc = (Button)findViewById(R.id.docActivityBtn);
+        btnDoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Doc.class);
+                startActivity(i);
+            }
+        });
+
+        btnMonAgenda = (Button)findViewById(R.id.myAgendaActivityBtn);
+        btnMonAgenda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MonAgenda.class);
+                startActivity(i);
+            }
+        });
+
+        btnProgram = (Button)findViewById(R.id.programActivityBtn);
+        btnProgram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Program.class);
+                startActivity(i);
+            }
+        });
+
     }
 
     @Override
