1f6f16c4dffb1ea671654f6af24e685e9a3a82f9
Thierry St-Pierre <Thierry.St-Pierre.1@collegeahuntsic.qc.ca>
2016-05-03 15:14:28 -0400

    Changé icônes, inséré logo dans bar Titre

--- app/src/main/java/com/devel/ccqf/ccqfmisson/MainActivity.java
+++ app/src/main/java/com/devel/ccqf/ccqfmisson/MainActivity.java
@@ -1,22 +1,47 @@
 package com.devel.ccqf.ccqfmisson;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ActionMenuView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ActionMenuView amvMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tToolbar);
+        amvMenu = (ActionMenuView) toolbar.findViewById(R.id.amvMenu);
+        amvMenu.setOnMenuItemClickListener(new ActionMenuView.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                return onOptionsItemSelected(menuItem);
+            }
+        });
         setSupportActionBar(toolbar);
+        Drawable logo = null;
+        logo = getResources().getDrawable(R.mipmap.ccqf_logo);
+        getSupportActionBar().setTitle(R.string.app_name);
+        getSupportActionBar().setLogo(logo);
+        for (int i = 0; i < toolbar.getChildCount(); i++) {
+            View child = toolbar.getChildAt(i);
+            if (child != null)
+                if (child.getClass() == ImageView.class) {
+                    ImageView iv2 = (ImageView) child;
+                    if (iv2.getDrawable() == logo) {
+                        iv2.setAdjustViewBounds(true);
+                    }
+                }
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -31,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main,  amvMenu.getMenu());
         return true;
     }
 
